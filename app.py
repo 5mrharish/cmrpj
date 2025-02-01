8f70b0bb787dccf5b128c46325cf03b92360f70d
5mrharish <152364578+5mrharish@users.noreply.github.com>
2025-02-01 08:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, jsonify, session, redirect, url_for, request, render_template_string
 import folium
-import random
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Required for sessions
@@ -23,19 +22,13 @@ def logout():
 
 @app.route('/map')
 def map_view():
-    
-    base_lat, base_lon = 17.6128, 78.4803  
-    lat = base_lat + random.uniform(-0.01, 0.01)
-    lon = base_lon + random.uniform(-0.01, 0.01)
+    lat, lon = 17.6128, 78.4803
 
-    # Create a Folium map centered 
     map = folium.Map(location=[lat, lon], zoom_start=15)
-    folium.Marker([lat, lon], popup="Random Location near Medchal, Hyderabad").add_to(map)
+    folium.Marker([lat, lon], popup="Medchal, Hyderabad").add_to(map)
 
-    # Generate the map HTML
     map_html = map._repr_html_()
 
-    # Render the map HTML within a simple template
     return render_template_string("""
         <!DOCTYPE html>
         <html lang="en">
